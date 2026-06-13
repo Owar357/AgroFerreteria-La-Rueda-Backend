e258c8f5cbff1548500d8d04c30c19a14ebb211e
0war357 <bart89761@gmail.com>
2026-06-12 23:49:19 -0600

    feat: se remplazo subototal en Ventas por gravador y exento

--- app/Http/Requests/Compra/StoreCompraRequest.php
+++ app/Http/Requests/Compra/StoreCompraRequest.php
@@ -23,34 +23,27 @@ class StoreCompraRequest extends FormRequest
     public function rules(): array
     {
         return [
-            // Compra
-            'tipo_dte' => 'required|string',
-            'numero_documento' => 'nullable|string|max:25',
-            'fecha_emision' => 'nullable|date',
-            'descuento_global' => 'nullable|numeric|min:0',
-            'iva_total' => 'nullable|numeric|min:0',
-            'monto_total' => 'nullable|numeric|min:0',
-            'estado_pago' => 'required|in:PAGADO,PENDIENTE,ABONADO,VENCIDO',
-            'fecha_vencimiento_pago' => 'nullable|date|after_or_equal:fecha_emision',
-            'proveedor_id' => 'required|exists:proveedores,id',
-            
+            'tipo_pago' => 'required|in:EFECTIVO,TARJETA,TRANSFERENCIA',
+            'estado' => 'sometimes|in:ANULADA',
+            'gravado' => 'required|numeric|min:0',
+            'exento' => 'sometimes|numeric|min:0',
+            'iva' => 'required|numeric|min:0',
+            'total' => 'required|numeric|min:0',
+            'efectivo_recibido' => 'nullable|numeric|min:0',
+            'cambio' => 'nullable|numeric|min:0',
+            'cliente_id' => 'nullable|exists:clientes,id',
+            'apertura_caja_id' => 'required|exists:turnos_caja,id',
 
-            // DetalleCompra
             'detalles' => 'required|array|min:1',
-            'detalles.*.cantidad_facturada' => 'nullable|numeric|min:0',
-            'detalles.*.cantidad_bonificada' => 'nullable|numeric|min:0',
-            'detalles.*.precio_unitario_factura' => 'nullable|numeric|min:0',
-            'detalles.*.iva_linea' => 'nullable|numeric|min:0',
-            'detalles.*.descuento_linea' => 'nullable|numeric|min:0',
-            'detalles.*.sub_total' => 'nullable|numeric|min:0',
-
-            // Lote
-            'detalles.*.lote.lote_fabricante' => 'nullable|string|max:50',
-            'detalles.*.lote.fecha_vencimiento' => 'nullable|date',
-            'detalles.*.lote.cantidad_inicial' => 'nullable|numeric|min:0',
-            'detalles.*.lote.costo_unitario_compra' => 'nullable|numeric|min:0',
-            'detalles.*.lote.porcentaje_descuento' => 'nullable|numeric|min:0',
-            'detalles.*.lote.presentacion_id' => 'required|exists:presentaciones,id',
+            'detalles.*.nombre_producto' => 'required|string',
+            'detalles.*.presentacion' => 'required|string',
+            'detalles.*.unidad_base' => 'required|string',
+            'detalles.*.cantidad' => 'required|numeric|min:0',
+            'detalles.*.precio_unitario' => 'required|numeric|min:0',
+            'detalles.*.subtotal' => 'required|numeric|min:0',
+            'detalles.*.iva_aplicado' => 'required|numeric|min:0',
+            'detalles.*.descuento_aplicado' => 'required|numeric|min:0',
+            'detalles.*.presentacion_id' => 'required|exists:presentaciones,id',
         ];
     }
 }
--- app/Http/Requests/Venta/StoreVentaRequest.php
+++ app/Http/Requests/Venta/StoreVentaRequest.php
@@ -22,29 +22,29 @@ class StoreVentaRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
-            'tipo_pago' => 'required|in:EFECTIVO,TARJETA,TRANSFERENCIA',
-            'estado' => 'sometimes|in:ANULADA',
-            'subtotal' => 'required|numeric|min:0',
-            'iva' => 'required|numeric|min:0',
-            'total' => 'required|numeric|min:0',
-            'efectivo_recibido' => 'nullable|numeric|min:0',
-            'cambio' => 'nullable|numeric|min:0',
-            'cliente_id' => 'nullable|exists:clientes,id',
-            'apertura_caja_id' => 'required|exists:turnos_caja,id',
+            return [
+                'tipo_pago' => 'required|in:EFECTIVO,TARJETA,TRANSFERENCIA',
+                'estado' => 'sometimes|in:ANULADA',
+                'subtotal' => 'required|numeric|min:0',
+                'iva' => 'required|numeric|min:0',
+                'total' => 'required|numeric|min:0',
+                'efectivo_recibido' => 'nullable|numeric|min:0',
+                'cambio' => 'nullable|numeric|min:0',
+                'cliente_id' => 'nullable|exists:clientes,id',
+                'apertura_caja_id' => 'required|exists:turnos_caja,id',
 
-            'detalles' => 'required|array|min:1',
-            'detalles.*.nombre_producto' => 'required|string',
-            'detalles.*.presentacion' => 'required|string',
-            'detalles.*.unidad_base'=> 'required|string', 
-            'detalles.*.cantidad' => 'required|numeric|min:0',
-            'detalles.*.precio_unitario' => 'required|numeric|min:0',
-            'detalles.*.subtotal' => 'required|numeric|min:0',
-            'detalles.*.iva_aplicado' => 'required|numeric|min:0',
-            'detalles.*.descuento_aplicado' => 'required|numeric|min:0',
-            'detalles.*.presentacion_id' => 'required|exists:presentaciones,id',
+                'detalles' => 'required|array|min:1',
+                'detalles.*.nombre_producto' => 'required|string',
+                'detalles.*.presentacion' => 'required|string',
+                'detalles.*.unidad_base'=> 'required|string', 
+                'detalles.*.cantidad' => 'required|numeric|min:0',
+                'detalles.*.precio_unitario' => 'required|numeric|min:0',
+                'detalles.*.subtotal' => 'required|numeric|min:0',
+                'detalles.*.iva_aplicado' => 'required|numeric|min:0',
+                'detalles.*.descuento_aplicado' => 'required|numeric|min:0',
+                'detalles.*.presentacion_id' => 'required|exists:presentaciones,id',
 
 
-        ];
+            ];
     }
 }
