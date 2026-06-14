1b36f48902215544a9bbaf7d823e79f42febf3ff
0war357 <bart89761@gmail.com>
2026-06-13 21:24:10 -0600

    fixed: se cambio elimino de la migracion de clientes el numero de telefono

--- database/migrations/2026_05_14_145336_create_clientes_table.php
+++ database/migrations/2026_05_14_145336_create_clientes_table.php
@@ -14,21 +14,22 @@ return new class extends Migration
         Schema::create('clientes', function (Blueprint $table) {
             $table->id();
             $table->enum('tipo_persona', ['JURIDICA', 'NATURAL']);
-            $table->string('nombre',250)->nullable();
-            $table->string('razon_social',250)->nullable();
+            $table->string('nombre',250)->nullable()->index();
+            $table->string('razon_social',250)->nullable()->index();
             $table->char('tipo_documento_receptor',2)->nullable()
         ->comment('13=DUI,36=NIT,02=Pasaporte,03=Carnet Residente');
-            $table->string('numero_documento', 20)->nullable()->index();
-            $table->string('nrc', 15)->nullable()->index();
+            $table->string('numero_documento', 20)->nullable();
+            $table->string('nrc', 15)->nullable();
             $table->string('cod_actividad',10)->nullable();
             $table->string('giro_actividad',250)->nullable();
+            
             $table->char('cod_departamento', 2)->nullable();
             $table->char('cod_municipio', 4)->nullable();
             $table->string('complemento', 250)->nullable();
-            $table->string('telefono', 20)->nullable();
+      
             $table->string('correo', 150)->nullable();
-            $table->boolean('activo')->default(true);
-            $table->foreignId('registrado_por')->constrained('users');
+            $table->boolean('activo')->default(true)->index();
+            $table->foreignId('registrado_por')->nullable()->constrained('users');
             $table->timestamps();
 
             $table->unique([
