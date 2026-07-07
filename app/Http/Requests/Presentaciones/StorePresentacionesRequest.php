14c9481adc46dfd41b7afa9b418ae9ff4cab4483
0war357 <bart89761@gmail.com>
2026-07-06 18:47:07 -0600

    feat:mejorar los request pendientes

--- app/Http/Requests/Presentaciones/StorePresentacionesRequest.php
+++ app/Http/Requests/Presentaciones/StorePresentacionesRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests\Presentaciones;
 
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
+use Override;
 
 class StorePresentacionesRequest extends FormRequest
 {
@@ -27,6 +28,23 @@ class StorePresentacionesRequest extends FormRequest
             "factor_conversion" => "required|numeric|min:0",
             "precio_venta" => "required|numeric|min:0",
             "producto_id" => "required|exists:productos,id"
+        ];
+    }
+
+    
+    public function messages() : array
+    {
+        return [
+          "nombre.required" => "El nombre de la presentación es requerido",
+          "nombre.max" => "El máximo de carácteres permitidos son 100",   
+          "factor_conversion.required" => "El factor de conversión es requerido",
+          "factor_conversion.numeric" => "No puede haber letras en el factor de conversión",
+          "factor_conversion.min" => "El factor de conversión no puede ser menor a 0",
+          "precio_venta.required" => "El precio de venta es requerido",
+          "precio_venta.numeric" => "El precio de venta solo puede contener valores numéricos",
+          "precio_venta.min" => "El precio de venta no puede ser negativo",
+          "producto_id.required" => "El producto asociado es requerido",
+          "producto_id.exists" => "El producto no existe"
         ];
     }
 }   
--- app/Http/Requests/Venta/StoreVentaRequest.php
+++ app/Http/Requests/Venta/StoreVentaRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests\Venta;
 
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
+use Override;
 
 class StoreVentaRequest extends FormRequest
 {
@@ -24,7 +25,6 @@ class StoreVentaRequest extends FormRequest
     {
         return [
             'tipo_pago' => 'required|in:EFECTIVO,TARJETA,TRANSFERENCIA',
-            'estado' => 'sometimes|in:ANULADA',
             'gravado' => 'required|numeric|min:0',   
             'exento' => 'required|numeric|min:0',
             'total' => 'required|numeric|min:0',
@@ -45,5 +45,38 @@ class StoreVentaRequest extends FormRequest
             'detalles.*.presentacion_id' => 'required|exists:presentaciones,id',
 
         ];
+
     }
+
+
+      public function messages(): array
+        {
+            return [
+            'tipo_pago.required' =>  'El tipo de pago es obligatorio',
+            'tipo_pago.in' => 'El tipo de pago debe ser EFECTIVO, TARJETA O TRANSFERENCIA',
+            'gravado.required' => 'El monto de gravado es obligatorio' ,
+            'exento.required' => 'El monto de exento es obligatorio',
+            'total.required' => 'El monto total es obligatorio',
+            'total.numeric' => 'El total debe ser un valor numérico',
+            'total.min' => 'El total no puede ser negativo',
+            'efectivo_recibido.numeric' => 'El efectivo recibido debe ser un valor numérico',
+            'efectivo_recibido.min' => 'El efectivo recibido no puede ser negativo',
+            'cambio.numeric' => 'El cambio debe ser un valor numérico ',
+            'cambio.min' => 'El cambio no puede ser negativo',
+            'cliente_id.exists' => 'El cliente seleccionado no existe',
+            'apertura_caja_id.required' => 'Hay que indicar una apertura de caja disponible',
+            'apertura_caja_id.exists' => 'Actualmente no existe una apertura de caja disponible',    
+            
+            'detalles.required' => 'Debe agregar al menos un producto a la venta',
+            'detalles.min' => 'Debe agregar al menos un producto a la venta',
+            'detalles.*.nombre_producto.required' => 'El nombre del producto es obligatorio',
+            'detalles.*.presentacion.required' => 'La presentación es obligatoria',
+            'detalles.*.unidad_base.required' => 'La unidad base es obligatoria',
+            'detalles.*.cantidad.required' => 'La cantidad es obligatoria',
+            'detalles.*.cantidad.min' => 'La cantidad no puede ser negativa',
+            'detalles.*.precio_unitario.required' => 'El precio unitario  es obligatorio',
+            'detalles.*.subtotal.required'   => 'El subtotal es obligatorio',
+            'detalles.*.presentacion_id.exists' => 'La presentación indicada del producto no existe'
+            ]; 
+        }
 }
