2898d33e33790cc7d7fc068ee4e9b76673018a02
0war357 <bart89761@gmail.com>
2026-09-13 21:18:44 -0600

    refactor: se mejoro el diseño del ticket

--- routes/api.php
+++ routes/api.php
@@ -20,15 +20,16 @@ use App\Http\Controllers\Reportes\Financieros\FlujoComprasVentasReporteControlle
 use App\Http\Controllers\Reportes\Financieros\MargenGananciaReporteController;
 use App\Http\Controllers\Reportes\Inventario\InventarioValorizadoReporteController;
 use App\Http\Controllers\Reportes\Inventario\ProductosPorVencerReporteController;
-use App\Http\Controllers\Reportes\ReporteVentas\ReporteFinancieroVentasController;
 use App\Http\Controllers\Reportes\Ventas\ProductosMasVendidosController;
 use App\Http\Controllers\Reportes\Ventas\ReporteVentasController;
 use App\Http\Controllers\Reportes\Ventas\ResumenVentasReportController;
+use App\Http\Controllers\Reportes\Ventas\TicketVentaController;
 use App\Http\Controllers\Reportes\Ventas\VentasComparativaReportController;
 use App\Http\Controllers\Reportes\Ventas\VentasPorCategoriaController;
 use App\Http\Controllers\Reportes\Ventas\VentasPorUsuarioController;
 use App\Http\Controllers\UnidadMedidaController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\VentaController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
@@ -68,10 +69,10 @@ Route::middleware('auth:api')->group(function () {
     Route::apiResource('proveedores', ProveedorController::class);
 
     Route::post('/caja/apertura', [CajaController::class, 'abrirCaja']);
-    Route::get('/caja/estado', [CajaController::class, 'estadoCaja']); // RUTA AGREGADA VERIFICA EL ESTADO DE LA CAJA, ME AYUDA EN EL FRONTEN
+    Route::get('/caja/estado', [CajaController::class, 'estadoCaja']);
     Route::post('/caja/venta/apertura', [CajaController::class, 'abrirVenta']);
     Route::post('/caja/venta/cuadre', [CajaController::class, 'cuadrarVenta']);
-    Route::get('/caja/resumen-turno', [CajaController::class, 'resumenTurno']); // RUTA AGREGADA
+    Route::get('/caja/resumen-turno', [CajaController::class, 'resumenTurno']);
     Route::patch('/caja/venta/cierre', [CajaController::class, 'cerrarVentaCaja']);
     Route::patch('/caja/movimientos/{movimiento}/anular', [MovimientoExternoCajaController::class, 'anularMovimiento']);
     Route::apiResource('caja/movimientoExterno', MovimientoExternoCajaController::class)->only(['index', 'store', 'show']);
@@ -81,39 +82,24 @@ Route::middleware('auth:api')->group(function () {
 
     Route::post('/ajuste-inventario', AjusteInventarioController::class);
 
-});
+    Route::get('/kardex/{producto}', KardexController::class);
 
-Route::get('/kardex/{producto}', KardexController::class);
+    Route::prefix('/reportes')->group(function () {
+        Route::get('/ticket/{id}', TicketVentaController::class);
+        Route::get('/inventario/valorizado', InventarioValorizadoReporteController::class);
+        Route::get('/productos-por-vencer', ProductosPorVencerReporteController::class);
+        Route::get('/compras/por-proveedor', ComprasPorProveedorReporteController::class);
+        Route::get('/caja/arqueo', ArqueoCajaReporteController::class);
+        Route::get('/ventas/resumen', ResumenVentasReportController::class);
+        Route::get('/ventas/resumen/comparativa', VentasComparativaReportController::class);
+        Route::get('ventas/usuarios', VentasPorUsuarioController::class);
+        Route::get('ventas/categorias', VentasPorCategoriaController::class);
+        Route::get('ventas/producto-mas-vendidos', ProductosMasVendidosController::class);
+        Route::get('/ventas', ReporteVentasController::class);
 
-Route::get('/reportes/ticket/{id}', [ReporteVentasController::class, 'ticket']);
-
-
-Route::prefix('/reportes')->group(function () {
-
-    Route::get('/inventario/valorizado', InventarioValorizadoReporteController::class);
-    Route::get('/productos-por-vencer', ProductosPorVencerReporteController::class);
-
-    Route::get('/compras/por-proveedor', ComprasPorProveedorReporteController::class);
-
-    Route::get('/caja/arqueo', ArqueoCajaReporteController::class);
-
-    Route::get('/ventas/resumen', ResumenVentasReportController::class);
-
-    Route::get('/ventas/resumen/comparativa', VentasComparativaReportController::class);
-
-    Route::get('ventas/usuarios', VentasPorUsuarioController::class);
-
-    Route::get('ventas/categorias', VentasPorCategoriaController::class);
-
-    Route::get('ventas/producto-mas-vendidos', ProductosMasVendidosController::class);
-
-    Route::get('/ventas', ReporteVentasController::class);
-
-    Route::prefix('/financieros')->group(function () {
-
-        Route::get('/flujo', FlujoComprasVentasReporteController::class);
-
-        Route::get('/margen', MargenGananciaReporteController::class);
+        Route::prefix('/financieros')->group(function () {
+            Route::get('/flujo', FlujoComprasVentasReporteController::class);
+            Route::get('/margen', MargenGananciaReporteController::class);
+        });
     });
-
 });
