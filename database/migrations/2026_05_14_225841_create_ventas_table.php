d9c53d2cfdfc9f4315431d98c22429a75343517d
0war357 <bart89761@gmail.com>
2026-09-21 14:19:07 -0600

    feat: se agrego una funcion en el kardexservice  y se ajusto la compra seeder

--- database/migrations/2026_05_14_225841_create_ventas_table.php
+++ database/migrations/2026_05_14_225841_create_ventas_table.php
@@ -13,7 +13,7 @@
         {
             Schema::create('ventas', function (Blueprint $table) {
                 $table->id();
-                $table->string('numero_factura',31)->unique();
+                $table->string('numero_factura',30);
                 $table->enum('tipo_pago',['EFECTIVO', 'TARJETA', 'TRANSFERENCIA']);
                 $table->enum('estado',['PROCESADA','ANULADA'])->default('PROCESADA')->index();
                 $table->decimal('gravado',15,2);
