92e8695a006dec06f1fdc938a913483f235b2ac6
0war357 <bart89761@gmail.com>
2026-09-23 11:39:36 -0600

    feat: se creo el archivo cors

--- database/migrations/2026_05_14_225841_create_ventas_table.php
+++ database/migrations/2026_05_14_225841_create_ventas_table.php
@@ -13,7 +13,6 @@
         {
             Schema::create('ventas', function (Blueprint $table) {
                 $table->id();
-                $table->string('numero_factura',30);
                 $table->enum('tipo_pago',['EFECTIVO', 'TARJETA', 'TRANSFERENCIA']);
                 $table->enum('estado',['PROCESADA','ANULADA'])->default('PROCESADA')->index();
                 $table->decimal('gravado',15,2);
